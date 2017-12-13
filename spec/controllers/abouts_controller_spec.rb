b9ecd06d12a04a7b91cd69e6d341eb2d0a7a6ddf
Scott Livingstone <scottwlivingstone@gmail.com>
2017-12-12 16:13:37 -0800

    Add tests for About controller and model

--- spec/controllers/abouts_controller_spec.rb
+++ spec/controllers/abouts_controller_spec.rb
@@ -1,19 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe AboutsController, type: :controller do
+  login_user
+  let(:portfolio) { subject.current_user.portfolios.create(url: 'mytest') }
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:attributes) do
+    { about_me: 'testing title',
+      about_me_brief: 'exiting stuff',
+      location: 'here' }
+  end
+
+  describe 'about#create' do
+    it 'Allows user to create a new project' do
+      post :create, params: { portfolio_id: portfolio.id,
+                              about: attributes }
+      expect(About.all.count).to eq(1)
+    end
+    it 'Redirects to about view' do
+      post :create, params: { portfolio_id: portfolio.id,
+                              about: attributes }
+      expect(response).to redirect_to(dashboard_index_path(menu_action: 'about'))
     end
   end
 
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
+  describe 'about#update' do
+    it 'Allows use to update project' do
+      post :create, params: { portfolio_id: portfolio.id,
+                              about: attributes }
+      patch :update, params: { portfolio_id: portfolio.id,
+                               id: About.first.id,
+                               about: { about_me: 'working' } }
+      expect(About.first.about_me).to eq('working')
+    end
+    it 'Redirects to project view' do
+      post :create, params: { portfolio_id: portfolio.id,
+                              about: attributes }
+      patch :update, params: { portfolio_id: portfolio.id,
+                               id: About.first.id,
+                               about: { about_me: 'working' } }
+      expect(response).to redirect_to(dashboard_index_path(menu_action: 'about'))
     end
   end
-
 end
--- spec/models/about_spec.rb
+++ spec/models/about_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe About, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  let(:portfolio) { user.portfolios.create(url: 'testing') }
+  let(:attributes) do
+    { about_me: 'testing title',
+      about_me_brief: 'exiting stuff',
+      location: 'here',
+      portfolio_id: portfolio.id }
+  end
+
+  describe '#create' do
+    it 'creates new about' do
+      About.create(attributes)
+      expect(About.all.count).to eq(1)
+    end
+  end
+
+  describe '#update' do
+    let(:about) { About.create(attributes) }
+    it 'updates a about' do
+      about.update(about_me: 'updated')
+      expect(About.first.about_me).to eq('updated')
+    end
+  end
+
+  describe '#portfolio' do
+    it { should belong_to(:portfolio) }
+  end
 end
