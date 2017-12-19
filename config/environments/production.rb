4aa25243c99304320372a19c9a8bc1806c1dcbf9
Scott Livingstone <scottwlivingstone@gmail.com>
2017-12-18 20:45:14 -0800

    Change MailGun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
-    domain: 'mg.mydevportal.io'
+    domain: 'mydevportal.io'
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
