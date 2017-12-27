35cbb4269435fcffe681ada8e9bdf2e5de046596
Scott Livingstone <scottwlivingstone@gmail.com>
2017-12-27 13:40:34 -0800

    Feature: Add/Edit/Delete JobApplications

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -45,14 +45,12 @@ class JobApplicationsController < ApplicationController
   # PATCH/PUT /job_applications/1
   # PATCH/PUT /job_applications/1.json
   def update
-    respond_to do |format|
-      if @job_application.update(job_application_params)
-        format.html { redirect_to @job_application, notice: 'Job application was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job_application }
-      else
-        format.html { render :edit }
-        format.json { render json: @job_application.errors, status: :unprocessable_entity }
-      end
+    if @job_application.update(job_application_params)
+      redirect_to user_job_applications_path(current_user)
+      flash[:notice] = 'Job Application successfully created!'
+    else
+      flash[:alert] = helpers.display_errors(@job_application)
+      render 'index'
     end
   end
 
@@ -69,6 +67,7 @@ class JobApplicationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_job_application
+      @portfolio = current_user.portfolios.first
       @job_application = JobApplication.find(params[:id])
     end
 
