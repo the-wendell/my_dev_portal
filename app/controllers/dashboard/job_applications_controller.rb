484979f46ad0b65e4c6c18649a544ac4a4f4a3d7
Scott Livingstone <scottwlivingstone@gmail.com>
2018-01-05 09:09:14 -0800

    Add seperate desc/asc option for filterrific

--- app/controllers/dashboard/job_applications_controller.rb
+++ app/controllers/dashboard/job_applications_controller.rb
@@ -1,46 +1,36 @@
 class Dashboard::JobApplicationsController < Dashboard::DashboardController
-  before_action :set_job_application, only: [:show, :edit, :update, :destroy]
+  before_action :set_job_application, only: %i[show edit update destroy]
+  before_action :combine_params, only: %i[index]
 
-  # GET /job_applications
-  # GET /job_applications.json
   def index
-    @portfolio = current_user.portfolios.first
-    # TODO: Set up global variable for portfolio
 
     @filterrific = initialize_filterrific(
       JobApplication,
-      params[:filterrific],
+      @filter_params,
       select_options: {
-        sorted_by: JobApplication.options_for_sorted_by
+        sorted_by: JobApplication.options_for_sorted_by,
+        asc_desc: JobApplication.options_for_asc_desc
       },
-      available_filters: [:sorted_by],
+      available_filters: %i[sorted_by]
     ) || return
     @job_applications = current_user.job_applications.filterrific_find(@filterrific)
 
     rescue ActiveRecord::RecordNotFound => e
       puts "Had to reset filterrific params: #{ e.message }"
-      redirect_to(reset_filterrific_url(format: :html)) and return
+      redirect_to(reset_filterrific_url(format: :html)) && return
   end
 
-  # GET /job_applications/1
-  # GET /job_applications/1.json
   def show
   end
 
-  # GET /job_applications/new
   def new
-    @portfolio = current_user.portfolios.first
     @job_application = JobApplication.new
   end
 
-  # GET /job_applications/1/edit
   def edit
   end
 
-  # POST /job_applications
-  # POST /job_applications.json
   def create
-    @portfolio = current_user.portfolios.first
     @job_applications = current_user.job_applications.all
     @job_application = JobApplication.new(job_application_params)
     @job_application.user = current_user
@@ -54,8 +44,6 @@ class Dashboard::JobApplicationsController < Dashboard::DashboardController
     end
   end
 
-  # PATCH/PUT /job_applications/1
-  # PATCH/PUT /job_applications/1.json
   def update
     if @job_application.update(job_application_params)
       redirect_to user_job_applications_path(current_user)
@@ -66,8 +54,6 @@ class Dashboard::JobApplicationsController < Dashboard::DashboardController
     end
   end
 
-  # DELETE /job_applications/1
-  # DELETE /job_applications/1.json
   def destroy
     @job_application.destroy
     respond_to do |format|
@@ -77,13 +63,20 @@ class Dashboard::JobApplicationsController < Dashboard::DashboardController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+    def combine_params
+      @filter_params = params[:filterrific]
+      
+      if @filter_params
+        @filter_params[:sorted_by] << @filter_params[:asc_desc]
+      end
+    end
+
     def set_job_application
       @portfolio = current_user.portfolios.first
       @job_application = JobApplication.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def job_application_params
       params.require(:job_application).permit(:first_contact_date, :company_name, :company_website, :job_location, :enthusiasm, :job_title, :job_link, :referral, :referral_type, :status, :industry, :notes)
     end
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,9 +1,11 @@
 class JobApplication < ApplicationRecord
   belongs_to :user
+  attr_accessor :direction
 
   filterrific(
-    default_filter_params: { sorted_by: 'first_contact_date_asc' },
+    default_filter_params: { sorted_by: 'first_contact_date_asc', asc_desc: 'asc' },
     available_filters: %i[
+      asc_desc
       sorted_by
       search_query
     ]
@@ -16,19 +18,44 @@ class JobApplication < ApplicationRecord
       order("job_applications.first_contact_date #{direction}")
     when /^company_name_/
       order("job_applications.company_name #{direction}")
+    when /^job_location_/
+      order("job_applications.job_location #{direction}")
+    when /^enthusiasm_/
+      order("job_applications.enthusiasm #{direction}")
+    when /^job_title_/
+      order("job_applications.job_title #{direction}")
+    when /^referral_/
+      order("job_applications.referral #{direction}")
+    when /^referral_type_/
+      order("job_applications.referral_type #{direction}")
+    when /^status_/
+      order("job_applications.status #{direction}")
+    when /^industry_/
+      order("job_applications.industry #{direction}")
     else
       raise(ArgumentError, "Invalid sort option: #{sort_option.inspect}")
     end
   }
 
-
-
   def self.options_for_sorted_by
     [
-      ['First Contact Date (newest)', 'first_contact_date_asc'],
-      ['First Contact Date (oldest)', 'first_contact_date_desc'],
-      ['Company Name (a-z)', 'company_name_asc'],
-      ['Company Name (z-a)', 'company_name_desc']
+      ['First Contact Date', 'first_contact_date_'],
+      ['Company Name', 'company_name_'],
+      ['Job Location', 'job_location_'],
+      ['Enthusiasm', 'enthusiasm_'],
+      ['Job Title', 'job_title_'],
+      ['Referral', 'referral_'],
+      ['Referral Type', 'referral_type_'],
+      ['Status', 'status_'],
+      ['industry', 'industry_']
     ]
   end
+
+  def self.options_for_asc_desc
+    [
+      ['Ascending', 'asc'],
+      ['Descending', 'desc']
+    ]
+  end
+
 end
