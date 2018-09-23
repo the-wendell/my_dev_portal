9999b25eb08a2ba7e96589f13cb9d12e61de35ef
AndyRosenberg <andynaterosenberg@gmail.com>
2018-09-22 20:28:34 -0700

    Fattened portfolio model, extracted from Frontend::PortfolioController

--- app/controllers/front_end/portfolio_controller.rb
+++ app/controllers/front_end/portfolio_controller.rb
@@ -1,13 +1,11 @@
 class FrontEnd::PortfolioController < ApplicationController
   layout 'dashboard', only: [:preview]
   rescue_from Exception, :with => :render_404
+  before_action :render_portfolio
 
-  def show
-    render_portfolio
-  end
+  def show; end
 
   def preview
-    render_portfolio
     @portfolios = current_user.portfolios.all
     @dashboard = 'portfolios'
     render 'dashboard/portfolios/preview'
@@ -17,47 +15,17 @@ class FrontEnd::PortfolioController < ApplicationController
 
   def render_portfolio
     @portfolio = Portfolio.find_by(url: params[:portfolio])
-    @portfolio_header = @portfolio.portfolio_header || filler_header
-    @projects = @portfolio.projects.all.order(:order)
-    @technologies = @portfolio.technologies.all
-    @about = @portfolio.about || filler_about
-    @first_name = @portfolio_header.header_one.split(' ').first rescue ''
 
-    if @portfolio.show_years_exp && @portfolio.show_proficiency
-      @show_technology = 'all'
-    elsif @portfolio.show_years_exp
-      @show_technology = 'years'
-    elsif @portfolio.show_proficiency
-      @show_technology = 'proficiency'
-    else
-      @show_technology = 'none'
-    end
+    @portfolio_header = @portfolio.render_header
+    @projects = @portfolio.render_projects
+    @technologies = @portfolio.render_technologies
+    @about = @portfolio.render_about
+    @first_name = @portfolio.render_first_name
+    @show_technology = @portfolio.render_show_technology
   end
 
   def render_404
     flash[:alert] = 'This Portfolio does not exist'
     redirect_to root_path
-  end
-
-  def filler_about
-    About.new(
-      about_me: "Go back to your dashboard and create your 'About Me'",
-      email: '',
-      github: '',
-      linkedin: '',
-      facebook: '',
-      twitter: '',
-      website: '',
-      about_me_brief: ''
-    )
-  end
-
-  def filler_header
-    PortfolioHeader.new(
-      header_one: "Go back to your dashboard and create your 'Portfolio Header'",
-      header_two: '',
-      header_one_color: '#000000',
-      header_two_color: '#000000'
-    )
   end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -12,4 +12,59 @@ class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :delete_all
   has_one :portfolio_header, dependent: :delete
   has_one :about, dependent: :delete
+
+  def render_header
+    portfolio_header || filler_header
+  end
+
+  def render_projects
+    projects.all.order(:order)
+  end
+
+  def render_technologies
+    technologies.all
+  end
+
+  def render_about
+    about || filler_about
+  end
+
+  def render_first_name
+    portfolio_header ? portfolio_header.header_one.split(' ').first : ''
+  end
+
+  def render_show_technology
+    if show_years_exp && show_proficiency
+      'all'
+    elsif show_years_exp
+      'years'
+    elsif show_proficiency
+      'proficiency'
+    else
+      'none'
+    end
+  end
+
+  private
+  def filler_header
+    PortfolioHeader.new(
+      header_one: "Go back to your dashboard and create your 'Portfolio Header'",
+      header_two: '',
+      header_one_color: '#000000',
+      header_two_color: '#000000'
+    )
+  end
+
+  def filler_about
+    About.new(
+      about_me: "Go back to your dashboard and create your 'About Me'",
+      email: '',
+      github: '',
+      linkedin: '',
+      facebook: '',
+      twitter: '',
+      website: '',
+      about_me_brief: ''
+    )
+  end
 end
